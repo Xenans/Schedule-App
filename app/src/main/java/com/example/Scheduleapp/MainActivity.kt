87d79e5fc8b184d8fe634ed900008b382bb3d79e
tnc1 <tnc1@sfu.ca>
2022-01-15 23:07:44 -0800

    moved recyclerview into fragment

--- app/src/main/java/com/example/Scheduleapp/MainActivity.kt
+++ app/src/main/java/com/example/Scheduleapp/MainActivity.kt
@@ -2,19 +2,13 @@ package com.example.scheduleapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-
-    private var layoutManager: RecyclerView.LayoutManager? = null
-    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,13 +24,6 @@ class MainActivity : AppCompatActivity() {
         bottomNavigationView.setupWithNavController(navController)
 
 
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
-        layoutManager = LinearLayoutManager(this)
-
-        recyclerView.layoutManager = layoutManager
-
-        adapter = RecyclerAdapter()
-        recyclerView.adapter = adapter
     }
 }
