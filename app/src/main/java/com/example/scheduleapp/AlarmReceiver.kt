89273b511faa2b713ffb7d168af48c9a15efc19c
TylerWon <tywon@shaw.ca>
2022-01-15 23:19:22 -0800

    implement push notifications

--- app/src/main/java/com/example/scheduleapp/AlarmReceiver.kt
+++ app/src/main/java/com/example/scheduleapp/AlarmReceiver.kt
@@ -1,4 +1,57 @@
 package com.example.scheduleapp
 
-class AlarmReceiver {
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+// Receives all notifications from app and broadcasts them out to user
+class AlarmReceiver : BroadcastReceiver() {
+    private var id: Int = 0         // ID of notification
+    private var rc: Int = 0         // Request code for Intent
+
+    // Callback for when notification is received
+    override fun onReceive(context: Context, intent: Intent) {
+        // Setup Intent so that when notification is clicked, it opens a new Activity
+        val newIntent = Intent(context, NotificationActivity::class.java)
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)       // why do this?
+
+        lateinit var pendingIntent: PendingIntent
+        if (Build.VERSION.SDK_INT >= 23) {
+            pendingIntent = PendingIntent.getActivity(context, this.rc, newIntent, PendingIntent.FLAG_IMMUTABLE)        // when Version >= 23, need to include mutability flag
+        } else {
+            pendingIntent = PendingIntent.getActivity(context, this.rc, newIntent, 0)
+        }
+
+        // Create the notification
+        val builder = NotificationCompat.Builder(context, context.getString(R.string.notification_channel_id))
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("Schedule App")
+            .setContentText(intent.getStringExtra("notificationMessage"))
+            .setAutoCancel(true)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+
+        // Show the notification
+        val notificationManagerCompat = NotificationManagerCompat.from(context)
+        notificationManagerCompat.notify(this.id, builder.build())
+
+        // Increment id and rc
+        incrementId()
+        incrementRc()
+    }
+
+    // Increments the value of id
+    private fun incrementId() {
+        id++
+    }
+
+    // Increments the value of rc
+    private fun incrementRc() {
+        rc++
+    }
 }
--- app/src/main/java/com/example/scheduleapp/MainActivity.kt
+++ app/src/main/java/com/example/scheduleapp/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.scheduleapp
 
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -10,11 +16,14 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private var layoutManager: RecyclerView.LayoutManager? = null
     private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+
+    private var rc : Int = 0        // request code for Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,5 +47,27 @@ class MainActivity : AppCompatActivity() {
 
         adapter = RecyclerAdapter()
         recyclerView.adapter = adapter
+
+        createNotificationChanel()
     }
+
+    // Creates a notification channel
+    private fun createNotificationChanel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = getString(R.string.notification_channel_name)
+            val descriptionText = getString(R.string.notification_channel_description)
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel(getString(R.string.notification_channel_id), name, importance).apply {
+                description = descriptionText
+            }
+
+            // Register the channel with the system
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+
 }
