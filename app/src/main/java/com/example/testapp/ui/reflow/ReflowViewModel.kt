4207fe4b28cde0a55a5dde2ca5db771df6fc2de7
tnc1 <tnc1@sfu.ca>
2022-01-15 16:59:09 -0800

    initial commit

---
+++ app/src/main/java/com/example/testapp/ui/reflow/ReflowViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.testapp.ui.reflow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ReflowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is reflow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
